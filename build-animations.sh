e11fb4b8d2fa1b916aecbc5685b1ab2b15c29655
Eli Heuer <elih@protonmail.com>
2024-07-19 23:34:50 -0700

    update pre-alpha animation source files

---
+++ build-animations.sh
@@ -0,0 +1,2 @@
+python documentation/animation/pre-alpha/anim-006/anim-006.py --output documentation/animation/pre-alpha/anim-006/anim-006.mp4
+open documentation/animation/pre-alpha/anim-006/anim-006.mp4
---
+++ build-wip.sh
@@ -0,0 +1,2 @@
+python documentation/animation/pre-alpha/wip-006/wip-006.py --output documentation/animation/pre-alpha/wip-006/wip-006.mp4
+open documentation/animation/pre-alpha/wip-006/wip-006.mp4
