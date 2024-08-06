1bb0f76af38a4c0ad6538053a357b159447ddb2a
Eli Heuer <elih@protonmail.com>
2024-08-05 19:29:51 -0700

    Update image source files

--- documentation/images/pre-alpha/wip-009.py
+++ documentation/images/pre-alpha/wip-009.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 # Width, Height, Margin, Unit, Frames
-W, H, M, U, F = 1080*2, 1080*2, 120, 30, 50
+W, H, M, U, F = 1080*2, 1080*2, 120, 60, 50
 MAIN_FONT_PATH = "fonts/RenaVF.ttf"
 MAIN_TEXT_OPSZ = 144
 CURRENT_DATE = datetime.now()
@@ -28,7 +28,8 @@ ttFont = TTFont(MAIN_FONT_PATH)
 
 # Draws a grid
 def grid():
-    db.stroke(0.9, 0.3, 0.0, 1)
+    db.fill(None)
+    db.stroke(0)
     db.strokeWidth(1)
     step_x = 0
     step_y = 0
@@ -40,8 +41,8 @@ def grid():
     for y in range(36*2):
         db.polygon((M, M + step_y), (W - M, M + step_y))
         step_y += increment_y
-    db.stroke(0.9, 0.0, 0.0, 1.0)
-    db.fill(None)
+    #db.stroke(0.9, 0.0, 0.0, 1.0)
+    #db.fill(None)
     db.polygon((W / 2, 0), (W / 2, H))
     db.polygon((0, H / 2), (W, H / 2))
 
@@ -67,60 +68,70 @@ def sin_loop(x):
 # Draw the page/frame and a grid if "GRID_VIEW" is set to "True"
 def draw_background():
     db.newPage(W, H)
-    db.fill(0.025)
-    db.fill(0.03)
     db.fill(0.05)
+    db.fill(0.55)
     db.rect(-2, -2, W + 2, H + 2)
+ 
+
+    im = db.ImageObject()
+    with im:
+        # set a size for the image
+        db.size(2160*2, 2160*2)
+        # draw something
+        #db.fill(0.1)
+        #db.rect(0, 0, 2160, 2160)
+        db.fill(0.8, 0.4, 0)
+        db.rect(0, 0, 1080*2, 1080*2)
+        db.fill(1, 0, 0)
+        db.rect(0, 0, 540*2, 540*2)
+    im.gaussianBlur(radius=300)
+    #im.boxBlur(radius=100)
+    db.blendMode("color")
+    db.image(im, (-300, -300))
+    db.blendMode("clear")
+    
+    #db.fill(0.6)
+    #db.rect(-2, -2, W + 2, H + 2)
+
     if GRID_VIEW:
         grid()
     else:
         pass
 
 
-# Set font and style before animation
-varWght = 0
-#step = -1
-step = 0
-phase = 0
-r,g,b = 0.75,0.75,0.75
-xpos = 0
-
-
 # Main Text
 draw_background()
-
 db.font(MAIN_FONT_PATH)
-db.fill(0.9)
-db.stroke(None)
-db.tracking(None)
-db.fontSize(180)
-db.fontVariations(opsz=144)
-db.fontVariations(wght=700)
 db.openTypeFeatures(dlig=False)
 #db.openTypeFeatures(dlig=True)
 #for axis, data in db.listFontVariations().items():
 #   print((axis, data))
 
-db.fontSize(195)
-#db.fill(0.03)
-#db.rect(0,0,W,U*5+1)
-db.fill(0.75)
-TOP_ROW = 58
-db.text("abcdefghijklmnopq", (M+(U*1), M+(U*(TOP_ROW-0))), align="left")
-db.text("rstuvwxyzß.,:;-–—!?", (M+(U*1), M+(U*(TOP_ROW-7))), align="left")
-db.text("ABCDEFGHIJKLMN", (M+(U*1), M+(U*(TOP_ROW-14))), align="left")
-db.text("OPQRSTUVWXYZ&", (M+(U*1), M+(U*(TOP_ROW-21))), align="left")
-db.text("1234567890₿$¢€¥", (M+(U*1), M+(U*(TOP_ROW-28))), align="left")
-db.text("ÁĂÂÄÀĀÅÃĄÆØŁ", (M+(U*1), M+(U*(TOP_ROW-35))), align="left")
-db.text("áăâäàāåãąæðęøœ", (M+(U*1), M+(U*(TOP_ROW-42))), align="left")
-db.text("(){}[]¡¿¶|/\\©®™«»", (M+(U*1), M+(U*(TOP_ROW-49))), align="left")
-db.text("+−×÷=<>§@%#*^", (M+(U*1), M+(U*(TOP_ROW-56))), align="left")
 
-#db.text("£ÇĐÐĘĦĮĽŁẞ", (M+(U*0), M+(U*12)), align="left")
-#db.text("ŊØŒÞŞẞßŲŒ", (M+(U*0), M+(U*12)), align="left")
-#db.text("ąæçďđðęģ", (M+(U*0), M+(U*12)), align="left")
-#db.text("#‚“„”‘'’\"", (M+(U*0), M+(U*12)), align="left")
-#db.text("øœ~°·•", (M+(U*0), M+(U*12)), align="left")
+db.stroke(None)
+db.tracking(None)
+SIZE = 109
+OPSZ = 144
+db.fontVariations(wght=700)
+db.fontVariations(opsz=OPSZ)
+db.fontSize(SIZE)
+db.lineHeight(SIZE*0.75)
+db.fill(0.1)
+LONG_TEXT_001 = "“Bitcoin is a swarm of cyber hornets serving the goddess of wisdom, feeding on the fire of truth, exponentially growing ever smarter, faster, and stronger behind a wall of encrypted energy.”\n\n—Michael Saylor"
+#db.textBox(LONG_TEXT_001,(M+(U*0),M-(U*10), U*32, U*32), align="left")
+#db.text(f"Rena 400 Pre-Alpha: opsz {OPSZ}", (M+(U*(0)), M+(U*(0))), align="left")
+
+
+db.text("Rena Bold 144", (M+(U*(1)), M+(U*(29.5))), align="left")
+TOP_ROW = 15
+db.text("“Bitcoin is a swarm of cyber hornets", (M+(U*(0.1)), M+(U*(TOP_ROW-0))), align="left")
+db.text("serving the goddess of wisdom,", (M+(U*(1)), M+(U*(TOP_ROW-2))), align="left")
+db.text("feeding on the fire of truth,", (M+(U*(1)), M+(U*(TOP_ROW-4))), align="left")
+db.text("exponentially growing ever", (M+(U*(0.95)), M+(U*(TOP_ROW-6))), align="left")
+db.text("smarter, faster, and stronger ", (M+(U*(0.975)), M+(U*(TOP_ROW-8))), align="left")
+db.text("behind a wall of encrypted energy.”", (M+(U*(1)), M+(U*(TOP_ROW-10))), align="left")
+db.text("—Michael Saylor", (M+(U*(1)), M+(U*(TOP_ROW-14))), align="left")
+
 
 db.saveImage(args.output)
 print("DrawBot: Done\n")
